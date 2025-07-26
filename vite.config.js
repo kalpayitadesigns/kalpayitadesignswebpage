365e5db4f8c672e5fd67dda43a65c049b98493ef
phanindrareddy2006 <2300032080@kluniversity.in>
2025-07-26 20:46:32 +0530

    V1.0.1

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/kalpayitadesignswebpage/', // ✔️ Correct base path
   plugins: [react()],
-  base: '/', // 👈 this is correct for Netlify
 })
