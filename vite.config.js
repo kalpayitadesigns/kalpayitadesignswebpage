eee931b6ef58a3869ddbf3137435b6c33f26cb65
phanindrareddy2006 <2300032080@kluniversity.in>
2025-07-26 20:15:39 +0530

    V1.0.1

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/kalpayitadesignswebpage/', // your repo name with slashes
+  base: '/kalpayitadesignswebpage/', // ✔️ Correct base path
   plugins: [react()],
 })
